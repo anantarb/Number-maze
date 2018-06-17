a00acdaf23e94918389c126c269d41906a202ed9
Ananta Raj Bhattarai <38761631+anantarb@users.noreply.github.com>
2018-06-17 11:43:54 +0200

    Update puzzle.h

--- puzzle.h
+++ puzzle.h
@@ -13,9 +13,14 @@ class PuzzleBoard{
         list<int> *adj;
     public:
         PuzzleBoard(int boardSize, int **fields = NULL);
+        /* If fields is null, then initialize the fields of the board with random values between 1 and boardSize-1. */
         bool makeMove(int direction);
+        /* makes the move (if valid), direction is 0 for up, 1 for right, 2 for down, 3 for left. Returns true if the move was 
+           valid, false otherwise. */
         bool getResult();
+        /* Returns false if game is not over yet, true if puzzle was solved */
         friend std::ostream &operator<<(std::ostream &os, PuzzleBoard const &m);
         int solve();
+        /* returns the minimum number of moves needed to solve the puzzle, and -1 if it’s not solvable. */
 };
 
